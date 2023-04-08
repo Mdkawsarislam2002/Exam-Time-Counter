47cc152a53413e9f34a29929f9afd36e1987c2f2
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2023-04-09 04:35:22 +0600

    setup for dev mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,11 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { invoke } from "@tauri-apps/api/tauri";
+import { NOTFOUND } from "dns";
+import Card from "./components/Card";
 
 function App() {
+  const [StoreData, setStoreData] = useState([]);
+
   const [greetMsg, setGreetMsg] = useState("");
   const [name, setName] = useState("");
 
@@ -10,9 +14,27 @@ function App() {
     setGreetMsg(await invoke("greet", { name }));
   }
 
+  const fetchStoreData = async () => {
+    fetch("https://fakestoreapi.com/products")
+      .then((res) => res.json())
+      .then((json) => setStoreData(json));
+  };
+
+  useEffect(() => {
+    fetchStoreData();
+  }, []);
+
   return (
     <div>
-      <h1 className="font-bold text-slate-600 text-2xl">Yeasin</h1>
+      <h1 className="font-bold text-slate-600 text-2xl">Store Data</h1>
+
+      <div className="flex flex-wrap">
+        {StoreData.map((item) => {
+          return (
+            <Card description={item.description} category={item.category} />
+          );
+        })}
+      </div>
     </div>
   );
 }
