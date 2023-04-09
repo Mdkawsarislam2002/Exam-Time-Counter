97660d7b374b09476b9d24d76dd3d9f5971c9eb8
Md Kawsar Islam Yeasin <mdkawsarislam2002@gmail.com>
2023-04-09 16:04:03 +0600

    added exam counter

--- src/App.tsx
+++ src/App.tsx
@@ -1,39 +1,44 @@
 import { useEffect, useState } from "react";
+import Card from "./components/Card";
+import { RootObject } from "./Types/AppTypes";
+
 import { invoke } from "@tauri-apps/api/tauri";
 import { NOTFOUND } from "dns";
-import Card from "./components/Card";
 
 function App() {
-  const [StoreData, setStoreData] = useState([]);
-
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
-
-  async function greet() {
-    // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
-  }
-
-  const fetchStoreData = async () => {
-    fetch("https://fakestoreapi.com/products")
-      .then((res) => res.json())
-      .then((json) => setStoreData(json));
-  };
+  const [timeLeft, setTimeLeft] = useState({
+    days: 0,
+    hours: 0,
+    minutes: 0,
+  });
 
   useEffect(() => {
-    fetchStoreData();
+    const intervalId = setInterval(() => {
+      const currentDate = new Date();
+      const targetDate = new Date("August 3, 2023");
+      const diffTime = targetDate.getTime() - currentDate.getTime();
+      const diffDays = Math.floor(diffTime / (1000 * 60 * 60 * 24));
+      const diffHours = Math.floor(
+        (diffTime % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+      );
+      const diffMinutes = Math.floor(
+        (diffTime % (1000 * 60 * 60)) / (1000 * 60)
+      );
+      setTimeLeft({ days: diffDays, hours: diffHours, minutes: diffMinutes });
+    }, 500);
+    return () => clearInterval(intervalId);
   }, []);
 
   return (
-    <div>
-      <h1 className="font-bold text-slate-600 text-2xl">Store Data</h1>
+    <div className="container bg-slate-50   w-screen h-screen ">
+      <h1 className="font-extrabold text-3xl text-center">Exam Time Left </h1>
 
-      <div className="flex flex-wrap">
-        {StoreData.map((item) => {
-          return (
-            <Card description={item.description} category={item.category} />
-          );
-        })}
+      <div className="font-semibold flex flex-col items-center justify-start text-xl mt-16 gap-3">
+        <div className="flex flex-col justify-start">
+          <p>Day Left : {timeLeft.days} </p>
+          <p>hour Left : {timeLeft.hours} </p>
+          <p>Minutes left: {timeLeft.minutes}</p>
+        </div>
       </div>
     </div>
   );
